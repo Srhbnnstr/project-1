1b2ea230a54f0a8a12f34544d71278766ab85f46
srhbnnstr <srhbnnstr@gmail.com>
2016-04-08 11:14:15 -0700

    removed project file

--- project/controllers/index.js
+++
@@ -1,4 +0,0 @@
-var mongoose = require ("mongoose");
-
-module.exports.api = require('./apiController');
-module.exports.animals = require('./animalController');
--- project/models/index.js
+++
@@ -1,6 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/anifull-test');
-
-var Animal = require('./animal');
-
-module.exports.Animal = Animal;
