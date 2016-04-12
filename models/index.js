9be7340f13eb9d1bb884cb24f34b0093b3ab6a8d
srhbnnstr <srhbnnstr@gmail.com>
2016-04-11 23:31:29 -0700

    heroku deply retry

--- models/index.js
+++ models/index.js
@@ -1,4 +1,6 @@
 var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost/anifull-test');
+mongoose.connect(process.env.MONGOLAB_URI ||
+                 process.env.MONGOHQ_URL ||
+                'mongodb://localhost/anifull-test');
 
 module.exports.Animal = require('./animals.js');
